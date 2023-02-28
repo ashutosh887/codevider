239f7ecb7714b362cb06f6d0216be0622aefde3a
Ashutosh Jha <ashutoshj887@gmail.com>
2023-02-28 21:44:43 +0530

    fix: less verbose

--- src/config/index.js
+++ src/config/index.js
@@ -4,8 +4,8 @@ import python from "../assets/python.png"
 import javascript from "../assets/javascript.png"
 
 export const logos = {
-    cpp: cpp,
-    java: java,
-    python: python,
-    javascript: javascript
+    cpp,
+    java,
+    python,
+    javascript
 }
