76649ae352e6520cd43f127fa77c56b06a49ccf1
Ashutosh Jha <ashutoshj887@gmail.com>
2023-02-28 21:42:29 +0530

    fix: lang icons

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+import cpp from "../assets/cpp.png"
+import java from "../assets/java.png"
+import python from "../assets/python.png"
+import javascript from "../assets/javascript.png"
+
+export const logos = {
+    cpp: cpp,
+    java: java,
+    python: python,
+    javascript: javascript
+}
